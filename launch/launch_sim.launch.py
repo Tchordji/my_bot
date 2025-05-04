7e2e6b6608d9af2a723e2034d7952dca8494d3b0
Robin Tchordjallian <robin.tchordjallian@gmail.com>
2025-05-04 11:46:47 +0200

    corrige certains problèmes mais on ne roule toujours pas

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -45,7 +45,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+                    launch_arguments={'gz_args': ['-r -v1 ', world], 'on_exit_shutdown': 'true'}.items()
              )
 
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
